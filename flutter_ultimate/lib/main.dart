91e8d5bac28a7e9441a57080426df8d1e95fa9d1
Levent C. <vrkafa@gmail.com>
2025-06-19 19:42:53 +0300

    appbar

--- flutter_ultimate/lib/main.dart
+++ flutter_ultimate/lib/main.dart
@@ -11,47 +11,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter web app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
+          seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter ultimate course'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter map'),
+          centerTitle: false,
+          leading: Icon(Icons.login),
+          actions: [Text('hjkfhaj'), Icon(Icons.login)],
+          backgroundColor: Colors.teal,
+        ),
       ),
-      body: Stack(
-        children: [
-          SizedBox(height: 300, child: Center(child: Text('Sizedbox'))),
-          ListTile(
-            leading: Icon(Icons.join_full),
-            tileColor: Colors.red,
-            title: Text('Listtile'),
-            trailing: Text('dfasdfas'),
-            onTap: () {},
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
