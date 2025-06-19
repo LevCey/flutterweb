85394d2ed5e2e2d1c20ae994914d5177b101e892
Levent C. <vrkafa@gmail.com>
2025-06-19 21:21:03 +0300

    recap

--- flutter_ultimate/lib/main.dart
+++ flutter_ultimate/lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:flutte_ultimate/widgets/navbar_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-String? title = 'Flutter map';
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,38 +23,10 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title!), centerTitle: true),
-      body: currentIndex == 0
-          ? Center(child: Text('1'))
-          : Center(child: Text('0')),
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-        onDestinationSelected: (int value) {
-          setState(() {
-            currentIndex = value;
-          });
-        },
-        selectedIndex: currentIndex,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter map')),
+        body: Container(),
+        bottomNavigationBar: NavbarWidget(),
       ),
     );
   }
