3a836b74ed36af1148d6ac3d4d8d0fd4b49b4889
Levent C. <vrkafa@gmail.com>
2025-06-21 00:13:04 +0300

    ui elements

--- flutter_ultimate/lib/views/pages/profile_page.dart
+++ flutter_ultimate/lib/views/pages/profile_page.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('profile page'));
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(border: OutlineInputBorder()),
+            onEditingComplete: () {
+              setState(() {});
+            },
+          ),
+          Text(controller.text),
+          Checkbox(
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text('click me'),
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          Switch(
+            value: isSwitched,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+          SwitchListTile.adaptive(
+            title: Text('Switch me'),
+            value: isSwitched,
+            onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+          Slider(
+            max: 10,
+            divisions: 10,
+            value: sliderValue,
+            onChanged: (double value) {
+              setState(() {
+                sliderValue = value;
+                print(value);
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
